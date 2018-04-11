78d62200aed64aa6c045be9d1aa5681d5543dee0
Metaflix <edmondson.kurt@gmail.com>
2018-04-11 01:02:31 +0000

    it

--- Mealplan.sql
+++ Mealplan.sql
@@ -3,17 +3,17 @@ use mealplan;
 
 create table useraccounts(
     useraccnum int auto_increment not null,
-    username varchar (50) not null, 
-    firstname varchar (50) not null, 
-    lastname varchar (50) not null, 
+    username varchar(50) not null, 
+    firstname varchar(50) not null, 
+    lastname varchar(50) not null, 
     dob date not null, 
-    age int (10) not null, 
-    email varchar (50) not null,  
-    phone varchar (30) not null,
-    gender varchar (10) not null,  
-    city varchar (100) not null
-    country varchar (30) not null, 
-    primary key (useraccnum, username) 
+    age int(10) not null, 
+    email varchar(50) not null,  
+    phone varchar(30) not null,
+    gender varchar(10) not null,  
+    city varchar(100) not null,
+    country varchar(30) not null, 
+    primary key (useraccnum) 
 ); 
 
 create table profiles( 
@@ -25,13 +25,13 @@ create table profiles(
     secondaryfoodchoice enum ("fruits", "vegetables", "dairy", "meats", "grains"), 
     tertiaryfoodchoice enum ("fruits", "vegetables", "dairy", "meats", "grains"), 
     primary key (useraccnum, username), 
-    foreign key (useraccnum) references useraccounts (useraccnum) on delete cascade
-    foreign key(username) references useraccounts (username) on delete cascade
+    foreign key (useraccnum) references useraccounts (useraccnum) on delete cascade,
+    foreign key(username) references useraccounts (username) on delete cascade,
     );  
     
 create table recipes( 
     recipenum int auto_increment not null, 
-    recipename varchar (200) not null,  
+    recipename varchar(200) not null,  
     recipecreatedate date not null,  
     recipetype enum
     primary key (recipenum)
